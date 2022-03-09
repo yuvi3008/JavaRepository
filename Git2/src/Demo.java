611ed9a5230b1456c709c336a7ef5b75b22432fb
t819856 <t819856@VW10A26958.principalusa.corp.principal.com>
2022-03-09 11:25:31 +0530

    New project git2 created.

---
+++ Git2/src/Demo.java
@@ -0,0 +1,12 @@
+import pack1.MyinterfaceImpl;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyinterfaceImpl impl = new MyinterfaceImpl();
+		impl.disp();
+
+	}
+
+}
---
+++ Git2/src/pack1/MyinterfaceImpl.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class MyinterfaceImpl implements MyInterface{
+
+	@Override
+	public void disp() {
+		// TODO Auto-generated method stub
+	System.out.println("inside MyInterfaceImpl class");
+	}
+
+		
+}
